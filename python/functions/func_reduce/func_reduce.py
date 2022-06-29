1310313d1503ae932ab4aff4d7cfc62e069f30db
abunbux <abunbux@gmail.com>
2022-06-29 19:26:40 +0300

    Ветку «python» вынес в отдельную ветку.

--- python/functions/func_reduce/func_reduce.py
+++
@@ -1,5 +0,0 @@
-from functools import reduce
-
-
-# print(reduce(lambda l, b: l * b, (87, 44, 9, 33)))
-print(reduce(lambda a, b: a + b, (1, 2, 3, 4, 5)))
--- python/functions/func_zip/func_zip.py
+++
@@ -1,4 +0,0 @@
-a = [1, 2, 3, 4, 5, 6, 7]
-b = 'abcdefg'
-result = zip(a, b)
-print(list(result))
